7bc53dca91b3ab487db20d268cb5795cbc9b88f3
Sab1tov <bestplayer0202@gmail.com>
2025-05-11 00:04:01 +0500

    fix/navbar

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -134,18 +134,18 @@ const Navbar = () => {
 														}`}
 														style={{ minHeight: '56px' }}
 													>
-														<span className='flex items-center justify-center w-10 h-10'>
+														<span className='flex items-center justify-center w-8 h-8'>
 															<svg
 																xmlns='http://www.w3.org/2000/svg'
-																width='32'
-																height='32'
+																width='24'
+																height='24'
 																viewBox='0 0 24 24'
 																fill='none'
 																stroke='currentColor'
 																strokeWidth='2'
 																strokeLinecap='round'
 																strokeLinejoin='round'
-																className='w-8 h-8'
+																className='w-6 h-6'
 															>
 																<path d='M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4'></path>
 																<polyline points='16 17 21 12 16 7'></polyline>
