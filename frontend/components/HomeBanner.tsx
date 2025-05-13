3944f3d5bdcd66bebd6e3682b7e44b7ac548af6e
Sab1tov <bestplayer0202@gmail.com>
2025-05-14 01:22:09 +0500

    fix/videos30

--- frontend/components/HomeBanner.tsx
+++ frontend/components/HomeBanner.tsx
@@ -52,28 +52,35 @@ const HomeBanner = () => {
 						className='relative max-w-xl opacity-50 outline-white outline-2 outline-offset-1 rounded-full'
 						onClick={focusSearchInput}
 					>
-						<div className='relative flex items-center bg-white bg-opacity-20 border border-white border-opacity-50 rounded-full overflow-hidden'>
+						<div className='flex items-center bg-white bg-opacity-20 border border-white border-opacity-50 rounded-full overflow-hidden'>
 							<input
 								ref={searchInputRef}
 								type='text'
 								placeholder='Күй немесе әннің атауын іздеу...'
-								className='flex-grow w-full bg-transparent text-black placeholder-white placeholder-opacity-50 px-4 py-3 sm:pl-6 sm:py-4 pr-12 focus:outline-none text-sm sm:text-base'
+								className='w-full bg-transparent text-black placeholder-white placeholder-opacity-50 px-4 py-3 sm:pl-6 sm:py-4 pr-12 focus:outline-none text-sm sm:text-base'
 								value={searchQuery}
 								onChange={e => setSearchQuery(e.target.value)}
 							/>
 							<button
 								type='submit'
-								className='absolute right-0 top-0 bottom-0 px-4 sm:px-6 py-3 sm:py-4 flex items-center justify-center'
+								className='absolute right-2 top-1/2 transform -translate-y-1/2'
 								aria-label='Поиск'
-								onClick={focusSearchInput}
 							>
-								<Image
-									src='/images/banner/search-icon.svg'
-									alt='Поиск'
-									width={20}
-									height={20}
-									className='opacity-50'
-								/>
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									width='22'
+									height='22'
+									viewBox='0 0 24 24'
+									fill='none'
+									stroke='white'
+									strokeWidth='2'
+									strokeLinecap='round'
+									strokeLinejoin='round'
+									className='feather feather-search'
+								>
+									<circle cx='11' cy='11' r='8'></circle>
+									<line x1='21' y1='21' x2='16.65' y2='16.65'></line>
+								</svg>
 							</button>
 						</div>
 					</form>
