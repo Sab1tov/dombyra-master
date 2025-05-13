9647c33265a74f33fef56cb5ed1de38356048351
Sab1tov <bestplayer0202@gmail.com>
2025-05-14 01:17:22 +0500

    fix/videos29

--- frontend/components/HomeBanner.tsx
+++ frontend/components/HomeBanner.tsx
@@ -67,29 +67,13 @@ const HomeBanner = () => {
 								aria-label='Поиск'
 								onClick={focusSearchInput}
 							>
-								<svg
-									width='20'
-									height='20'
-									viewBox='0 0 24 24'
-									fill='none'
-									xmlns='http://www.w3.org/2000/svg'
-									className='sm:w-6 sm:h-6 opacity-50'
-								>
-									<path
-										d='M11 19C15.4183 19 19 15.4183 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19Z'
-										stroke='white'
-										strokeWidth='2'
-										strokeLinecap='round'
-										strokeLinejoin='round'
-									/>
-									<path
-										d='M21 21L16.65 16.65'
-										stroke='white'
-										strokeWidth='2'
-										strokeLinecap='round'
-										strokeLinejoin='round'
-									/>
-								</svg>
+								<Image
+									src='/images/banner/search-icon.svg'
+									alt='Поиск'
+									width={20}
+									height={20}
+									className='opacity-50'
+								/>
 							</button>
 						</div>
 					</form>
