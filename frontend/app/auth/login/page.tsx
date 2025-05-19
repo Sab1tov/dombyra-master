ca2600bb3e2df01a53cd9b77f01fb7470aacfeb5
Sab1tov <bestplayer0202@gmail.com>
2025-05-19 21:46:04 +0500

    fix/reset4

--- frontend/app/auth/login/page.tsx
+++ frontend/app/auth/login/page.tsx
@@ -1,25 +1,15 @@
 'use client'
 
 import { useModal } from '@/components/ModalManager'
-import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
 
 export default function LoginPage() {
 	const { openModal } = useModal()
-	const router = useRouter()
 
 	useEffect(() => {
-		// Проверяем, если нас перенаправили с какой-то другой страницы (с параметрами или без)
-		setTimeout(() => {
-			// Открываем модальное окно логина
-			openModal('login')
-			// Перенаправляем на главную страницу только после открытия модального окна
-			setTimeout(() => {
-				router.push('/')
-			}, 100)
-		}, 100)
-	}, [openModal, router])
+		openModal('login')
+	}, [openModal])
 
-	// Эта страница не будет отображаться, так как происходит перенаправление
+	// Эта страница не будет отображаться, так как происходит открытие модального окна
 	return null
 }
